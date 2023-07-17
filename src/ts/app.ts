1c7678f1dd8b46a4fd2af95307c91ffdbab6b4f6
colorincode <colorincodeco@gmail.com>
2023-07-16 19:21:50 -0600

    update app, tsconfig, pck json
    updated package json, type defs, tsconfig, and app.ts to be able to take type defs and defs for jquery

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,6 +4,12 @@
 //commonJS module
 //const script = require(‘./js/script’);
 
+// import jquery 
+import * as jquery from 'jquery';
+
+// will need this for build to compile correctly 
+
+(window as any).$ = (window as any).jQuery = jquery;
 
 console.log("stepped through imports, help me");
 
